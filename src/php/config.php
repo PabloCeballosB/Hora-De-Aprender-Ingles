fd27ba497f1aad615059ff570bc9fd07067c7bcf
PabloCeballosB <90768884+PabloCeballosB@users.noreply.github.com>
2022-06-13 11:45:01 +0200

    Delete src directory

--- src/php/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //Acceso Base de Datos
-  define('servidorbd', 'localhost');
-  define('basedatos', 'horadeaprenderingles');
-  define('usuario', 'admin1');
-  define('contrasena', '1234');
-?>
