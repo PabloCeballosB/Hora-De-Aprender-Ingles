2c7ce43121c71d5b8cd1c839a33a870350abc6a5
PabloCeballosB <90768884+PabloCeballosB@users.noreply.github.com>
2022-06-13 11:45:38 +0200

    Falta Listar y comprobar datos

---
+++ src/php/Controlador/controlador.php
@@ -0,0 +1,61 @@
+<?php
+
+    class Controlador{
+      private $modelo;
+      public $filas;
+      public $idFrase;
+
+      function __construct(){
+          require_once('../Modelo/modelo.php');
+          $this -> modelo = new Modelo;
+      }
+
+      /*Funcion que se encarga de mostrar la pagina de AltaFrase.php y comprobar
+      si se ha introducido datos correctamente asi enviando a la funcion
+      "insertar" de modelo.php*/
+      function altaFrase(){
+          require_once('../Vista/AltaFrase.php');
+          if(isset($_POST["btnterminar"]) && !empty($_POST["btnterminar"])){
+              $this -> modelo -> altaDatos();
+              $this -> idFrase = $this -> modelo -> idFrase;
+          }
+      }
+
+
+      function insertarDatos(){
+          require_once('../Vista/HacerEjercicio.php');
+            $this->modelo->insertarDatos();
+            $this->filas = $this -> modelo -> filas;
+      }
+
+      function hacerEjercicio(){
+        require_once('../Vista/HacerEjercicio.php');
+        $this -> modelo -> subirResultados();
+      }
+  }
+
+  $controlador = new Controlador;
+
+  /*Switch que se encarga de mandar a una funcion u otra depende del click donde se haya realizado*/
+  switch ($_GET['click']) {
+      case 'altaFrase':
+          $controlador->altaFrase();
+          break;
+      case 'hacerEjercicio':
+          $controlador->hacerEjercicio();
+          break;
+      /*case 'listafrase':
+          $controlador -> listarVista();
+          break;
+      case 'modificarfrase':
+          $controlador -> modificarVista();
+          break;
+      case 'borrarfrase':
+          $controlador -> borrar();
+          break;*/
+      default:
+          echo "<h1>Pagina no Encontrada</h1>";
+          break;
+  }
+
+?>
---
+++ src/php/config.php
@@ -0,0 +1,7 @@
+<?php
+  //Acceso Base de Datos
+  define('servidorbd', 'localhost');
+  define('basedatos', 'horadeaprenderingles');
+  define('usuario', 'admin1');
+  define('contrasena', '1234');
+?>
